48e906644c602a9186909655c5fceb1f04b14d76
hide1713 <hide1713@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-02 20:25:41 +0000

    Simple file transfer server and client. use 4kB buffer, tested with 100MB files

--- trunk/src/client.c
+++ trunk/src/client.c
@@ -1,8 +1,94 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <getopt.h>
+
+#define BUFFERSIZE 4096
+
+int main(int argc, char **argv)
 {
-        printf("reliable udp client\n");
+	char ch, *filename = (char*) "receivefile.txt", *src_ip_str = (char*) "127.0.0.1";
+	int port = 5320;
+
+	while ((ch = getopt(argc, argv, "f:p:a:")) != -1) {
+		switch (ch) {
+		case 'a':
+			src_ip_str = optarg;
+			break;
+		case 'f':
+			filename = optarg;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case '?':
+			if (optopt == 'f' || optopt == 'p' || optopt == 'a') {
+				fprintf(stderr, "Option -%c requires an argument\n", optopt);
+			}
+			else {
+				fprintf(stderr, "Unknown option\n");
+			}
+			exit(0);
+		}
+	}
+
+	struct sockaddr_in src_addr, self_addr;
+	int sock;
+
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		fprintf(stderr, "Error: can not open socket\n");
+		exit(EXIT_FAILURE);
+	}
+
+	memset((char *)&self_addr, 0, sizeof(self_addr));
+	self_addr.sin_family = AF_INET;
+	self_addr.sin_port = htons(port);
+	self_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (struct sockaddr *) &self_addr, sizeof(self_addr)) == -1) {
+		fprintf(stderr, "Error: can not bind socket\n");
+		exit(1);
+	}
+
+	memset((char *)&src_addr, 0, sizeof(src_addr));
+	src_addr.sin_family = AF_INET;
+	src_addr.sin_port = htons(port);
+	if (inet_aton(src_ip_str, &src_addr.sin_addr) == 0) {
+		fprintf(stderr, "Error: invalid source address\n");
+		exit(1);
+	}
+
+	char receive_buf[BUFFERSIZE];
+	FILE *fp;
+	int i, src_addr_len = sizeof(src_addr);
+
+	if ((fp = fopen(filename, "w")) == NULL) {
+		fprintf(stderr, "Error: can not open file\n");
+		exit(1);
+	}
+
+	printf("Got prepared to receive file\n");
+	
+	int flag = 1, recv_size=0, write_size=0;
+	while ((recv_size=recvfrom(sock, receive_buf, BUFFERSIZE, 0, (struct sockaddr *) &src_addr, &src_addr_len)) > 0) {
+		//printf("receiving %s from %s:%d\n", filename, src_ip_str, htons(src_addr.sin_port));
+		//printf("recv_size %d\n",recv_size);
+		do{
+			write_size=fwrite(receive_buf,1,recv_size-write_size,fp);
+		}
+		while(write_size!=recv_size);
+		//printf("write_size %d\n",write_size);
+		write_size=0;
+		if (recv_size!=BUFFERSIZE)
+		{
+			fclose(fp);
+			break;
+		}
+		
+	}
 }
-
-
 
--- trunk/src/server.c
+++ trunk/src/server.c
@@ -1,5 +1,80 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <getopt.h>
+
+#define BUFFERSIZE 4096
+
+int main(int argc, char **argv)
 {
-	printf("reliable udp server\n");
+	char ch, *filename = (char*) "sendfile.txt", *dst_ip_str = (char*) "127.0.0.1";
+	int port = 5320;
+
+	while ((ch = getopt(argc, argv, "f:p:a:")) != -1) {
+		switch (ch) {
+		case 'a':
+			dst_ip_str = optarg;
+			break;
+		case 'f':
+			filename = optarg;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case '?':
+			if (optopt == 'f' || optopt == 'p' || optopt == 'a') {
+				fprintf(stderr, "Option -%c requires an argument\n", optopt);
+			}
+			else {
+				fprintf(stderr, "Unknown option\n");
+			}
+			exit(0);
+		}
+	}
+
+	struct sockaddr_in dst_addr;
+	int sock;
+
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		fprintf(stderr, "Error: can not open socket\n");
+		exit(EXIT_FAILURE);
+	}
+
+	memset((char *)&dst_addr, 0, sizeof(dst_addr));
+	dst_addr.sin_family = AF_INET;
+	dst_addr.sin_port = htons(port);
+	if (inet_aton(dst_ip_str, &dst_addr.sin_addr) == 0) {
+		fprintf(stderr, "Error: invalid destination address\n");
+		exit(1);
+	}
+
+	char send_buf[BUFFERSIZE];
+	FILE *fp;
+	int i,send_byte=0,read_byte=0;
+
+	if ((fp = fopen(filename, "r")) == NULL) {
+		fprintf(stderr, "Error: can not open file\n");
+		exit(1);
+	}
+
+	while ((read_byte=fread(send_buf,1,BUFFERSIZE,fp))>0) {
+		do{
+			send_byte+=sendto(sock, send_buf, read_byte-send_byte, 0, (struct sockaddr *)&dst_addr, sizeof(dst_addr));
+		}	
+		while(send_byte!=read_byte);
+		//printf("send_byts %d, read_byte %d\n",send_byte,read_byte);
+		send_byte=0;
+		usleep(10000); 	/* Sleep after send, give receiver little more time */
+	}
+
+	//printf("%s\n",send_buf);
+	printf("Complete sending %s to %s:%d\n", filename, dst_ip_str, port);
+	fclose(fp);
 }
+
+
