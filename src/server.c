a6ba3a412418db2fef8da5b866c04d225493ba73
sunchaomark <sunchaomark@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-03 22:34:24 +0000

    added Jacobson/Karel timeout

--- src/server.c
+++ src/server.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <getopt.h>
-
-#define BUFFERSIZE 4096
+#include "server.h"
 
 int main(int argc, char **argv)
 {
---
+++ src/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_HEADER
+#define SERVER_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <getopt.h>
+#include "timeout.h"
+
+#define BUFFERSIZE 4096
+
+#endif 
