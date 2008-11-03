eff1d04d72b19e8e9359a9164927f8068d36cb61
sunchaomark <sunchaomark@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-03 22:34:24 +0000

    added Jacobson/Karel timeout

--- trunk/config.h
+++ trunk/config.h
@@ -1,1 +1,23 @@
 /* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "reliable_udp"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "Lei Chen, Chao Sun, Azadeh Nematzadeh lc6@indiana.edu"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "reliable_udp"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "reliable_udp 0.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "reliable_udp"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1"
+
+/* Version number of package */
+#define VERSION "0.0.1"
