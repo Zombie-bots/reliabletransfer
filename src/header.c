884f45e77ba8b0298ff8599947bc667caf758f7e
hide1713 <hide1713@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-04 23:16:56 +0000

    Add functions related with packet(fill header, read header, fill packet, read packet) and Test code for those functions

--- src/header.c
+++ src/header.c
@@ -1,12 +1,38 @@
 #include "header.h"
+#include <assert.h>
 /* fill header without checksum */
-int fill_header(u_short seq, u_short ack, u_short offset, u_short flag, u_char* buffer)
+int fill_header(u_short seq, u_short ack, u_short offset, u_short flag, packet_t* packet)
 {
-	header_t h;
-	h.seq=seq;
-	h.ack=ack;
-	h.offset=offset;
-	h.flag=flag;
-	memcpy(buffer,h,sizeof(header_t));
+	assert(offset<=1024);
+	assert(flag==ACK||flag==FIN||flag==(ACK|FIN));
+	assert(packet!=0);
+	packet->header.seq=seq;
+	packet->header.ack=ack;
+	packet->header.offset=offset;
+	packet->header.flag=flag;
+	return 0;
+}
+
+int fill_packet(u_char* src, packet_t* packet,u_short size)
+{
+	assert(size<=PAYLOAD_SIZE);
+	memcpy(packet->payload,src,size);
+	return 0;
+}
+
+int read_header(header_t * p, packet_t* packet)
+{
+	header_t * tmp= (header_t*)packet;
+	p->seq=tmp->seq;
+	p->ack=tmp->ack;
+	p->offset=tmp->offset;
+	p->flag=tmp->flag;
+	return 0;
+}
+
+int read_packet(u_char* dest, packet_t* packet, u_short size)
+{
+	assert(size<=PAYLOAD_SIZE);
+	memcpy(dest,packet->payload,size);
 	return 0;
 }
