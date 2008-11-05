9017a0a5fa30a443aad0e25fa00b8967bbb8b4c6
hide1713 <hide1713@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-05 21:34:38 +0000

    Add timout code and test

---
+++ trunk/src/timeout.c
@@ -0,0 +1,32 @@
+#ifndef TIMEOUT_HEADER
+//#include "timeout.h"
+#endif
+
+extern int EST_RTT;
+extern int DEV;
+
+int timeout(int sam_rtt)
+{
+  sam_rtt -= EST_RTT;            //diff
+  EST_RTT += (sam_rtt >> 3);     //mul. delta
+  if (sam_rtt < 0)
+    sam_rtt = -sam_rtt;          // abs(diff)
+  sam_rtt -= DEV;                // diff - DEV
+  DEV += (sam_rtt >> 2);         // mul. detal
+  return (EST_RTT + (DEV << 2));
+}
+
+/*
+main ()
+{
+  int sam_rtt;
+
+  printf("Input a sample RTT: ");
+  scanf("%d",&sam_rtt);
+  while(sam_rtt != 0) {
+    printf("Timeout result: %d\n", timeout(sam_rtt));
+    printf("Input a sample RTT: ");
+    scanf("%d",&sam_rtt);
+  }
+}
+*/
---
+++ trunk/src/timeout.h
@@ -0,0 +1,7 @@
+#ifndef TIMEOUT_HEADER
+#define TIMEOUT_HEADER
+int EST_RTT = 0;
+int DEV = 0;
+
+int timeout(int sam_rtt);
+#endif
