226375420994f30aae2ff81a145c233497ef64a1
azadeh.n <azadeh.n@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-09 23:52:41 +0000

    sliding window sender part is finished

--- test/sw_test.c
+++ test/sw_test.c
@@ -1,43 +1,52 @@
 /*This file test the sliding window algorithm
  * */
 #include <stdio.h>
-#include <../src/sw.c>
 #include <assert.h>
-#define FALSE 0
-#define TRUE 1
-extern last_packet_send;
-extern last_packet_ack;
-extern send_window;
+#include "../src/sw.h"
+
+extern short last_packet_send;
+extern short last_packet_ack;
+extern short send_window;
+extern short initial_seq_number;
 
 int main(int argc, char *argv[])
 {
-	last_packet_send=0;
+
+/*	last_packet_send=0;
 	last_packet_ack=0;
-	send_window=4;
-	
-	int seq_num=1000;
+	send_window=4;*/
+	int seq_num=initial_seq_number;
+	int ack_num=initial_seq_number;
 	int ret_val=FALSE;
 	int i;
+	enum ack ack_result;
 	for (i=0;i<4;i++)
 	{
-		ret_val=sender_send_packet(seq);
+		ret_val=sender_send_packet(seq_num);
+		printf("i %d val %d \n", i,ret_val);
 		assert(ret_val==TRUE);
-		seq++;
+		seq_num++;
 	}
 	/* Sender already sent 4 packet, can not send any more */
-	ret_val=sender_send_packet(seq);
+	ret_val=sender_send_packet(seq_num);
+	printf(" val %d \n", ret_val);
+
 	assert(ret_val==FALSE);
 	/* Receive one ack*/
-	sender_receive_ack();
-	ret_val=sender_send_packet(seq);
+	ack_result=sender_receive_ack(++ack_num);
+	printf(" ack result  %d \n", ack_result);
+
+	assert(ack_result==correct_ack);
+
+	ret_val=sender_send_packet(seq_num);
 	assert(ret_val==TRUE);
 	/* Send one packet, Then should not send anymore */
-	ret_val=sender_send_packet(seq);
+	ret_val=sender_send_packet(seq_num);
 	assert(ret_val==FALSE);
-	return 0
-	
-	       
-	
-	
-	
+	return 0;
+
+
+
+
+
 }
