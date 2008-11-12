28bbe70e6c7048a642f4dac316fc8fa3f17fdaab
hide1713 <hide1713@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-12 21:24:16 +0000

    Fix Makefile.am ,Now we can compile client and sever

--- trunk/src/udp_output.h
+++ trunk/src/udp_output.h
@@ -3,6 +3,8 @@
 #include "header.h"
 #include "checksum.h"
 #include "link_list.h"
+#include <sys/socket.h>
 
-int rudp_send(int socket, const void *buffer, size_t length, int flags);
+int rudp_send(int socket, char *buffer, size_t length, int flags,\
+	      const struct sockaddr *to, socklen_t tolen);
 #endif
