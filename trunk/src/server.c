6bf9e982151d3b5342925eef25184718e2fa352c
hide1713 <hide1713@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-13 05:01:22 +0000

    Fix client.c bugs.We can tranfer file with our send and recv now

--- trunk/src/server.c
+++ trunk/src/server.c
@@ -2,72 +2,67 @@
 
 int main(int argc, char **argv)
 {
-  char ch, *filename = (char*) "sendfile.txt";
-  char *src_ip_str = (char*) "127.0.0.1", *dst_ip_str = (char*) "127.0.0.1";
-  int port = 5320;
+	char ch, *filename = (char*) "sendfile.txt", *dst_ip_str = (char*) "127.0.0.1";
+	int port = 5320;
 
-  while ((ch = getopt(argc, argv, "f:p:a:s:")) != -1) {
-    switch (ch) {
-      case 'a':
-        dst_ip_str = optarg;
-        break;
-      case 's':
-        src_ip_str = optarg;
-        break;
-      case 'f':
-        filename = optarg;
-        break;
-      case 'p':
-        port = atoi(optarg);
-        break;
-      case '?':
-        if (optopt == 'f' || optopt == 'p' || optopt == 'a' || optopt == 's') {
-          fprintf(stderr, "Option -%c requires an argument\n", optopt);
-        } else {
-          fprintf(stderr, "Unknown option\n");
-        }
-        exit(0);
-    }
-  }
+	while ((ch = getopt(argc, argv, "f:p:a:")) != -1) {
+		switch (ch) {
+		case 'a':
+			dst_ip_str = optarg;
+			break;
+		case 'f':
+			filename = optarg;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case '?':
+			if (optopt == 'f' || optopt == 'p' || optopt == 'a') {
+				fprintf(stderr, "Option -%c requires an argument\n", optopt);
+			}
+			else {
+				fprintf(stderr, "Unknown option\n");
+			}
+			exit(0);
+		}
+	}
 
-  struct sockaddr_in dst_addr;
-  int sock;
+	struct sockaddr_in dst_addr;
+	int sock;
 
-  if ((sock = socket(AF_INET,SOCK_DGRAM, 0)) == -1) {
-    fprintf(stderr, "Error: can not open socket\n");
-    exit(EXIT_FAILURE);
-  }
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		fprintf(stderr, "Error: can not open socket\n");
+		exit(EXIT_FAILURE);
+	}
 
-  memset((char *) &dst_addr, 0, sizeof(dst_addr));
-  dst_addr.sin_family = AF_INET;
-  dst_addr.sin_port = htons(port);
-  if (inet_aton(dst_ip_str, &dst_addr.sin_addr) == 0) {
-    fprintf(stderr, "Error: invalid destination address\n");
-    exit(1);
-  }
+	memset((char *)&dst_addr, 0, sizeof(dst_addr));
+	dst_addr.sin_family = AF_INET;
+	dst_addr.sin_port = htons(port);
+	if (inet_aton(dst_ip_str, &dst_addr.sin_addr) == 0) {
+		fprintf(stderr, "Error: invalid destination address\n");
+		exit(1);
+	}
 
-  char send_buf[BUFFERSIZE];
-  FILE *fp;
-  int send_byte = 0, read_byte = 0;
+	char send_buf[BUFFERSIZE];
+	FILE *fp;
+	int send_byte=0,read_byte=0;
 
-  if ((fp = fopen(filename, "r")) == NULL) {
-    fprintf(stderr, "Error: can not open file\n");
-    exit(1);
-  }
+	if ((fp = fopen(filename, "r")) == NULL) {
+		fprintf(stderr, "Error: can not open file\n");
+		exit(1);
+	}
 
-  while ((read_byte = fread(send_buf, 1, BUFFERSIZE,fp)) > 0) {
-    do {
-      send_byte += rudp_send(sock, send_buf, read_byte - send_byte, 0,
-      (struct sockaddr *) &dst_addr, sizeof(dst_addr));
-    }
-    while (send_byte != read_byte);
-    //printf("send_byts %d, read_byte %d\n",send_byte,read_byte);
-    send_byte = 0;
-    usleep(10000); /* Sleep after send, give receiver little more time */
-  }
+	while ((read_byte=fread(send_buf,1,BUFFERSIZE,fp))>0) {
+		send_byte+=rudp_send(sock, send_buf, read_byte-send_byte, 0, (struct sockaddr *)&dst_addr, sizeof(dst_addr));
+		printf("send_byts %d, read_byte %d\n",send_byte,read_byte);
+		send_byte=0;
+		usleep(1000); 	/* Sleep after send, give receiver little more time */
+	}
 
-  //printf("%s\n",send_buf);
-  printf("Complete sending %s to %s:%d\n", filename, dst_ip_str, port);
-  fclose(fp);
-  return 0;
+	//printf("%s\n",send_buf);
+	printf("Complete sending %s to %s:%d\n", filename, dst_ip_str, port);
+	fclose(fp);
+	return 0;
 }
+
+
--- trunk/src/udp_input.c
+++ trunk/src/udp_input.c
@@ -3,20 +3,24 @@
 int rudp_recv(int sock, char *receive_buf, struct sockaddr_in *self_addr,
               struct sockaddr *src_addr, unsigned *src_addr_len, size_t *recv_size) {
   size_t size;
-  int flag;
+  char * p=receive_buf;
   char buffer[PACKET_SIZE];
-
+  header_t head;
   do {
     size = recvfrom(sock, (void *)buffer, PACKET_SIZE, 0, src_addr, src_addr_len);
     //assert (add_checksum(size, (u_char *)&(((struct sockaddr_in *)src_addr)->sin_addr.s_addr),
       //      (u_char *)&(self_addr->sin_addr.s_addr), size%2, (u_short *)buffer) == 0);
-    strncat(receive_buf, buffer+HEADER_SIZE, size-HEADER_SIZE-1);
-    printf("receive packet size %d\n",size);
-    *recv_size += size-HEADER_SIZE;
-    printf("receive buffer size %d\n",*recv_size);
-    receive_buf[*recv_size-1] = buffer[size-HEADER_SIZE-1];
-    flag = ((packet_t *)buffer)->header.flag;
-    printf("flag %u\n",flag);
-  } while (*recv_size < BUFFERSIZE && flag != FIN);
-  return flag;
+    read_header(&head,(packet_t *)buffer);
+    printf("Seq %d Ack %d Offset %d, Flag %d \n",head.seq,head.ack,head.offset,head.flag);
+    read_packet(p,(packet_t*)buffer,head.offset);
+    p+=head.offset;
+    *recv_size += head.offset;
+    //printf("receive buffer size %d\n",*recv_size);
+    if (head.flag==FIN)
+    {
+	    return head.flag;
+    }
+    
+  } while (*recv_size < BUFFERSIZE);
+  return head.flag;
 }
