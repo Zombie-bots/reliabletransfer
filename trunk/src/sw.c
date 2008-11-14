a170ad9e52675883b5581400cc2d77308d5115fe
azadeh.n <azadeh.n@8788e0c6-a893-11dd-9a07-0f3041a8e97c>
2008-11-14 23:51:09 +0000

    Add sw algorithm

---
+++ trunk/src/sw.c
@@ -0,0 +1,171 @@
+#include "sw.h"
+#include <assert.h>
+//extern short send_congestion_window;
+//extern short initial_seq_number;
+/*
+ * sender sequence variable
+ */
+
+
+/*
+ * receiver sequence  variable
+ */
+//extern short  rcvWindow; //receive window
+//extern short last_byte_received; //receive next packet , last byte written
+//extern short next_byte_expected;
+
+/*
+ * this function finds minimum value between two variable
+ */
+/*
+ * sender sequence variable
+ */
+short last_packet_sent=1000;  //send next packet
+short last_packet_acked=1000; //last byte ack
+short send_window=4;  //advertised  window, max rcv buffer, last bye written
+
+short send_congestion_window=6;
+
+
+short initial_seq_number=1000;
+
+
+/*
+ * receiver sequence  variable
+ */
+short rcvWindow=4; //receive window
+short last_byte_received=1003; //receive next packet , last byte written
+short next_byte_expected=1000;
+short init_rec_seq_number=0;
+
+
+
+
+short min(int x,int y)
+{
+	if(send_window<=send_congestion_window){
+		return send_window;
+	}
+	else{
+		return send_congestion_window;
+	}
+}
+/*
+ * update sliding window variables when sender send packet
+ */
+int sender_send_packet( short send_seq_number){
+	 printf("error1111! lastsen: %d, lastAck: %d , seq se:  %d", last_packet_sent, last_packet_acked,send_seq_number);
+
+    if (last_packet_sent-last_packet_acked < min(send_window,send_congestion_window))
+        {
+			last_packet_sent+=1;
+	    	// printf("error1! lastsen: %d, lastAck: %d ", last_packet_sent, last_packet_acked);
+	    	  printf("\n error1!");
+
+			return 1;
+        }
+    else{
+    	  printf("\n error2!");
+    	  return 0;
+    }
+}
+
+/*
+ * update sliding window variables when sender receive acknowledge
+ */
+enum ack sender_receive_ack( short send_seq_number){
+	printf("\n eeeeeeerror 0  senSq:  %d, lastAck: %d , last sen:  %d!", send_seq_number, last_packet_acked, last_packet_sent);
+
+	if ((send_seq_number == last_packet_acked +1) && (last_packet_acked<last_packet_sent) && (last_packet_sent<=(send_window+last_packet_acked))){
+						last_packet_acked=last_packet_acked+1;
+						printf("error 3!");
+
+						return correct_ack;
+					}
+	 else if (last_packet_acked<send_seq_number && send_seq_number<(send_window+last_packet_acked)){
+	 		printf("error2!");
+
+		 return in_window_ack;
+	 		}
+
+	 else if (send_seq_number<last_packet_acked && send_seq_number > (send_window+last_packet_acked)){
+			printf("error1!");
+
+		 return out_window_ack;
+			}
+
+
+}
+
+/*
+ * update sliding window variables when receiver receive packet
+ */
+enum recPkt receiver_receive_packet(short recieve_seq_number){
+	//assert(next_byte_expected<=last_byte_received);
+	printf("\n eeeeeeeeeeerror0! expect: %d , lastrec: %d, sqrec %d", next_byte_expected, last_byte_received, recieve_seq_number);
+	if (init_rec_seq_number==0){
+
+		init_rec_seq_number=recieve_seq_number;
+	}
+	if (next_byte_expected==last_byte_received){
+			last_byte_received=last_byte_received+1;
+			printf("error1!");
+
+	}
+	if (recieve_seq_number >  rcvWindow ||recieve_seq_number == rcvWindow || recieve_seq_number < next_byte_expected || recieve_seq_number > last_byte_received ){
+		printf("error2!");
+
+		return dropPkt;
+	}
+
+	if (next_byte_expected==recieve_seq_number){
+			  next_byte_expected=next_byte_expected+1;
+				printf("error3!");
+
+			  return acceptPkt;
+		      }
+
+	if(next_byte_expected<recieve_seq_number && recieve_seq_number<last_byte_received){
+		printf("error4!");
+
+		  return dropPkt;
+	}
+
+ }
+/*
+int main(void) {
+
+	int temp;
+	last_packet_sent=initial_seq_number;
+	last_packet_acked=initial_seq_number;
+	last_byte_received=initial_seq_number;
+
+	temp=sender_send_packet(0, 0);
+	printf("return : %d",temp);
+	//sender_receive_ack(0,0);
+	temp=sender_send_packet(0, 0);
+	printf("return : %d",temp);
+	//sender_receive_ack(0,0);
+	temp=sender_send_packet(0, 0);
+	printf("return : %d",temp);
+	//sender_receive_ack(0,0);
+	temp=sender_send_packet(0, 0);
+	printf("return : %d",temp);
+	//sender_receive_ack(0,0);
+
+	temp=sender_send_packet(0,0);
+	printf("return : %d",temp);
+
+	temp=sender_send_packet(0,0);
+		printf("return : %d",temp);
+	printf("last_packet_sent: %d",last_packet_sent);
+	sender_receive_ack(0,0);
+	sender_receive_ack(0,0);
+
+	temp=sender_send_packet(0,0);
+	printf("return : %d",temp);
+	sender_receive_ack(0,0);
+
+	return EXIT_SUCCESS;
+}
+*/
--- trunk/test/sw_test.c
+++ trunk/test/sw_test.c
@@ -9,6 +9,11 @@ extern short last_packet_ack;
 extern short send_window;
 extern short initial_seq_number;
 
+
+extern short rcvWindow;
+extern short last_byte_received;
+extern short next_byte_expected;
+
 int main(int argc, char *argv[])
 {
 
@@ -20,6 +25,8 @@ int main(int argc, char *argv[])
 	int ret_val=FALSE;
 	int i;
 	enum ack ack_result;
+	enum recPkt rpResult;
+
 	for (i=0;i<4;i++)
 	{
 		ret_val=sender_send_packet(seq_num);
@@ -30,23 +37,31 @@ int main(int argc, char *argv[])
 	/* Sender already sent 4 packet, can not send any more */
 	ret_val=sender_send_packet(seq_num);
 	printf(" val %d \n", ret_val);
-
 	assert(ret_val==FALSE);
-	/* Receive one ack*/
-	ack_result=sender_receive_ack(++ack_num);
-	printf(" ack result  %d \n", ack_result);
 
+	// Receive one ack
+	ack_num=ack_num+1;
+	ack_result=sender_receive_ack(ack_num);
+	printf(" ack result  %d \n", ack_result);
 	assert(ack_result==correct_ack);
 
 	ret_val=sender_send_packet(seq_num);
 	assert(ret_val==TRUE);
-	/* Send one packet, Then should not send anymore */
+	// Send one packet, Then should not send anymore
 	ret_val=sender_send_packet(seq_num);
 	assert(ret_val==FALSE);
+
+
+	//receiver
+	for (i=0;i<4;i++)
+		{
+			rpResult=receiver_receive_packet(seq_num+i);
+			printf("\n ))))))))))rec i %d val %d \n", i,rpResult);
+			assert(rpResult==acceptPkt);
+			//rec_sqnum++;
+		}
+		rpResult=receiver_receive_packet(seq_num);
+		printf("rec i %d val %d \n", i,rpResult);
+		assert(rpResult==dropPkt);
 	return 0;
-
-
-
-
-
 }
